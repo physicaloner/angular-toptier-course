9ba2d7de6c8194b3b0494f14ef46d508134091f1
physicaloner <allstar.perfect@gmail.com>
2020-08-08 11:01:00 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface CustomType {
+  counter: number;
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toptier-angular-course';
+  customValue: CustomType = { counter: 0, message: '' };
+
+  onClickMessage() {
+    if (this.customValue.counter < 10) {
+      this.customValue.counter += 1;
+    } else if (this.customValue.counter < 20) {
+      this.customValue.counter += 2;
+    } else if (this.customValue.counter < 30) {
+      this.customValue.counter += 5;
+    } else {
+      this.customValue.counter = 0;
+    }
+  }
 }
