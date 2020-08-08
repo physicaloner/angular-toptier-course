6a2c7304e815a2e5ac898ad2b0707dd508dfa2cd
physicaloner <allstar.perfect@gmail.com>
2020-08-08 12:11:40 +0700

    update with form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
 
 interface CustomType {
-  counter: number;
-  message: string;
+  index: number;
 }
 
 @Component({
@@ -10,19 +10,34 @@ interface CustomType {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  counter: CustomType[] = [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }, { index: 5 }];
   title = 'toptier-angular-course';
-  customValue: CustomType = { counter: 0, message: '' };
+  form: FormGroup;
+
+  constructor(private formBuiler: FormBuilder) {
+    this.form = this.formBuiler.group({
+      counterArray: this.formBuiler.array([]),
+      username: new FormControl(''),
+      password: new FormControl('')
+    });
+    this.counter.forEach(f => {
+      this.addFormArray(f);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  addFormArray(value: CustomType) {
+    this.getFormArray().push(this.formBuiler.group(value));
+  }
+
+  getFormArray(): FormArray {
+    return this.form.get('counterArray') as FormArray;
+  }
 
   onClickMessage() {
-    if (this.customValue.counter < 10) {
-      this.customValue.counter += 1;
-    } else if (this.customValue.counter < 20) {
-      this.customValue.counter += 2;
-    } else if (this.customValue.counter < 30) {
-      this.customValue.counter += 5;
-    } else {
-      this.customValue.counter = 0;
-    }
+    console.log(this.form.value);
   }
 }
